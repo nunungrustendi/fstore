736971a677a6a4c25ec542a9eaf4d3bedabb2b04
nunung <nunungrustendi@gmail.com>
2018-07-22 05:51:35 -0700

    database

--- config/database.php
+++ config/database.php
@@ -13,8 +13,9 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
+	
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -58,9 +59,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'foris'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'nunungrustendi771019'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
